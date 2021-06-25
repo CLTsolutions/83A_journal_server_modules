dd076d22c3ffdd7ec425e706329cdf0b7d93af19
Chelsey Tschida <chelsey.tschida@outlook.com>
2021-06-25 14:56:55 -0400

    added notes through 4.2

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
+// app.js is the entrance into this application
 const Express = require('express')
 // creating an instance of express (firing off a top-level Express() fn exported by Express module.
 // Allows us to create an Express app.
 const app = Express()
 
-// res.send is an Express fn
-// res (response) handles packaging the response obj
-// .send() method's job is to send off the response
-app.use('/test', (req, res) => {
-  res.send('This is a message from the test endpoint on the server!')
-})
+// // res.send is an Express fn
+// // res (response) handles packaging the response obj
+// // .send() method's job is to send off the response
+// app.use('/test', (req, res) => {
+//   res.send('This is a message from the test endpoint on the server!')
+// })
+
+// importing the controllers as a bundle through the obj we exported in the index.js file
+// - it's being stored it in a variable called controllers
+const controllers = require('./controllers')
+
+// calling app.use and using /journal as the first parameter
+// base URL is now http://localhost:3000/journal
+// for the second parameter for the use() fn, we pass in the controllers obj and use dot notation
+// - to access the desired journalController
+app.use('/journal', controllers.journalController)
 
 app.listen(3000, () => {
   console.log(`[Server]: App is listening on 3000.`)
